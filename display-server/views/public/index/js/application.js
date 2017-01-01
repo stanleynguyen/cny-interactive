c30a06d7d61ec4c63ae7dcabe92be29670bdb16b
gabrielwong159 <gabrielwong159@gmail.com>
2017-01-01 17:08:38 +0800

    Added div for lanterns, displays 5 wishes at once

--- display-server/views/public/index/js/application.js
+++ display-server/views/public/index/js/application.js
@@ -1,11 +1,22 @@
 var counter; //global counter to detect if there's new wish
 var cachedWishes; //global object to cache wishes
-var $textObj = $("#wish-text");
-var $wishContainer = $("#wish-container");
+
+var numLanterns = 5;
+var lanternArray = [$("#lantern1"), $("#lantern2"), $("#lantern3"), $("#lantern4"), $("#lantern5")];
+var lanternTextArray = [$("#lantern1-text"), $("#lantern2-text"), $("#lantern3-text"), $("#lantern4-text"), $("#lantern5-text")];
 
 $(document).ready(function() {
-  fetchWishes(displayText);
+  positionLanterns();
+  setInterval(fetchWishes.bind(event, displayText), 6000);
 });
+
+function positionLanterns() {
+  var spacing = 100;
+  var offset = 100;
+  var lanternWidth = 200;
+
+  for (var i=0; i<numLanterns; i++) lanternArray[i][0].style.left = spacing + ((offset + lanternWidth) * i) + 'px';
+}
 
 function fetchWishes(callback) {
   $.ajax({
@@ -27,16 +38,19 @@ function displayText(err, wishes) {
     counter = counter || wishes.length;
     cachedWishes = wishes;
   }
-  if (wishes.length > counter) {
-    counter++;
-    wish = wishes[counter-1].content;
-  } else {
-    randomIndex = Math.round(Math.random() * ( counter - 1 ));
-    wish = wishes[randomIndex].content;
+
+  for (var i=0; i<numLanterns; i++) {
+    if (wishes.length > counter) {
+      counter++;
+      wish = wishes[counter-1].content;
+    } else {
+      randomIndex = Math.round(Math.random() * ( counter - 1 ));
+      wish = wishes[randomIndex].content;
+    }
+
+    lanternTextArray[i].text(wish);
+
+    var speed = 5000 + (Math.random()*1000);
+    lanternArray[i].animate({top: '-200px'}, speed, function() { this.style.top = '100%'; });
   }
-	$textObj.text(wish);
-	$wishContainer.hide()
-    .fadeIn(1000)
-    .delay(3000)
-    .fadeOut(1000, fetchWishes.bind(event, displayText));
 }
