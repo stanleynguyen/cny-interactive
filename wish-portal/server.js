f68ad84921ff7d0a8202cd6ef5f3a7254ee31e78
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-01-03 14:28:33 +0800

    update whitelist

--- wish-portal/server.js
+++ wish-portal/server.js
@@ -17,7 +17,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('./views/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
-  let allowedOrigins = ["http://cny-interactive.herokuapp.com", "http://chinatownfestivals.sg"];
+  let allowedOrigins = [
+    "http://cny-interactive.herokuapp.com",
+    "http://chinatownfestivals.sg",
+    "http://www.chinatownfestivals.sg"
+  ];
   let origin = req.headers.origin;
   if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
