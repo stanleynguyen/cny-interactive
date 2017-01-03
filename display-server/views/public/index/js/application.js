bb310156cb18d6573063b855f8fbac1ff39b70b5
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-01-03 23:08:35 +0800

    quick style fix before release

--- display-server/views/public/index/js/application.js
+++ display-server/views/public/index/js/application.js
@@ -1,6 +1,8 @@
 var counter; //global counter to detect if there's new wish
 var cachedWishes; //global object to cache wishes
 var prevOffsetLeft;
+var isChineseAndShort;
+var isChineseAndLong;
 
 var lanternCounter = 0;
 var $wishContainer = $('#wish-container');
@@ -43,18 +45,29 @@ function generateLantern(err, wishes) {
     wish = wishes[randomIndex];
   }
 
+  isChineseAndShort = wish.content.match(/[\u3400-\u9FBF]/) && wish.content.length < 40;
+  isChineseAndLong = wish.content.match(/[\u3400-\u9FBF]/) && wish.content.length > 40;
   $wishContainer.append($(lantern(lanternCounter, wish)))
     .promise()
-    .done(animateLaterns.bind(null, lanternCounter));
+    .done(animateLaterns.bind(null, lanternCounter, isChineseAndShort, isChineseAndLong));
 }
 
-function animateLaterns(id) {
+function animateLaterns(id, isChineseAndShort, isChineseAndLong) {
   var thisLantern = $('#lantern-' + id);
-  var newOffsetLeft = Math.random() * (window.innerWidth - 200);
-  while (prevOffsetLeft && Math.abs(newOffsetLeft - prevOffsetLeft) < 200) newOffsetLeft = Math.random() * (window.innerWidth - 200);
+  var newOffsetLeft = Math.random() * (window.innerWidth - 350);
+  while (prevOffsetLeft && Math.abs(newOffsetLeft - prevOffsetLeft) < 350) newOffsetLeft = Math.random() * (window.innerWidth - 350);
   prevOffsetLeft = newOffsetLeft;
   thisLantern[0].style.left = newOffsetLeft + 'px';
-  thisLantern[0].style.zIndex = Math.round(Math.random() * 0.8);
+  var zIndex = Math.round(Math.random() * 0.8) + 1;
+  thisLantern[0].style.zIndex = zIndex;
+  if (zIndex === 1) {
+    thisLantern[0].style.width = '250px';
+    thisLantern[0].style.minHeight = '350px';
+    thisLantern[0].style.fontSize = '22px';
+  }
+  if (isChineseAndShort) thisLantern[0].style.fontSize = '26px';
+  else if (isChineseAndLong && zIndex === 1) thisLantern[0].style.fontSize = '20px';
+  else if (isChineseAndLong && zIndex === 2) thisLantern[0].style.fontSize = '22px';
   var duration = 30000 + (Math.random() * 5000);
   thisLantern.animate(
     {top: - thisLantern.height() + 'px'},
